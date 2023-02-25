ff087c733b7ae862feb387f7a9e92e99ef90250b
arnel <arnelgilbuela09@gmail.com>
2023-02-25 13:51:43 +0800

    add model to views

--- views.py
+++ views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 projects = ["sample 1", "sample 2", "sample 3"]
 # Create your views here.
 
 def index(request):
     return render(request, "portfolio/index.html", {
-        "projects": projects
+        "projects": Project.objects.all()
+        
     })
